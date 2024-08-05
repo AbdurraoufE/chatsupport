e13719972df358479234124794cb07be8e22aad1
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2024-08-05 12:31:22 -0600

    added openai to project

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,5 +1,22 @@
-import { NextResponse } from "next/server";
-export function POST(req){
-    console.log(req);
-    return NextResponse.json({message: "Hello from the server!"});
+import { NextResponse } from "next/server"
+import OpenAI from "openai";
+
+export async function POST(req){
+    const openai = new OpenAI();
+    const data = await req.json()
+
+    const completion = await openai.chat.completions.create({
+        messages: [{"role": "system", 
+            content: "You are a helpful assistant."},
+            {"role": "user", 
+                content: "Who won the world series in 2020?"},
+            {"role": "assistant", 
+                content: "The Los Angeles Dodgers won the World Series in 2020."},
+            {"role": "user", 
+                content: "Where was it played?"}],
+        model: "gpt-3.5-turbo",
+      });
+    
+    console.log(completion.choices[0].message.content);
+    return NextResponse.json({message: "Hola amigo from the server"})
 }
