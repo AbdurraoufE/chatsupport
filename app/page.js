08aaf2b609605c14a070b7bedbf3125bd1517e6b
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2024-08-07 18:23:24 -0600

    added functional bot to reply

--- app/page.js
+++ app/page.js
@@ -1,23 +1,71 @@
-"use client" 
+"use client";
 import Image from "next/image";
 import { Box, Stack, TextField, Button } from "@mui/material";
-import { useState} from "react"
+import { useState } from "react";
 
 export default function Home() {
-  const [messages, setMessages] = useState([{
-    role: "assistant",
-    content: `Hi! I am your virtual support agent. How can I help you today?`
-  }]);
+  const [messages, setMessages] = useState([
+    {
+      role: "assistant",
+      content: `Hi! I am your virtual support agent. How can I help you today?`,
+    },
+  ]);
 
-  const [message, setMessage] = useState("") //what user types in the chat box
+  const [message, setMessage] = useState(""); //what user types in the chat box
+
+  const sendMessage = async () => {
+    const userMessage = message;
+    setMessage(""); // when you send a message, the textbox will automatically be empty
+
+    // Add the user message
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { role: "user", content: userMessage },
+      { role: "assistant", content: "" },
+    ]);
+
+    //fetches the response from the server
+    const response = fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify([...messages, { role: "user", content: userMessage }]), //send another user message
+      // we get the response from the server
+    }).then(async (res) => {
+      //read it
+      const reader = res.body.getReader();
+      const decoder = new TextDecoder(); //decode it because we encoded it in the backend
+
+      let result = "";
+      return reader.read().then(function processText({ done, value }) {
+        if (done) {
+          return result; //
+        }
+        const text = decoder.decode(value || new Int8Array(), { stream: true });
+        setMessages((prevMessages) => {
+          let lastMessage = prevMessages[prevMessages.length - 1]; //last msg
+          let otherMessages = prevMessages.slice(0, prevMessages.length - 1); //gets all messages except the last one
+          return [
+            ...otherMessages,
+            {
+              ...lastMessage,
+              content: lastMessage.content + text,
+            },
+          ];
+        });
+        return reader.read().then(processText);
+      });
+    });
+  };
 
   return (
-    <Box 
-      width="100vw" 
-      height="100vh" 
-      display="flex" 
-      flexDirection="column" 
-      justifyContent="center" 
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
       alignItems="center"
     >
       <Stack
@@ -39,9 +87,9 @@ export default function Home() {
           {
             // display out messages
             messages.map((message, index) => (
-              <Box 
-                key={index} 
-                display="flex" 
+              <Box
+                key={index}
+                display="flex"
                 justifyContent={
                   message.role === "assistant" ? "flex-start" : "flex-end"
                 }
@@ -50,8 +98,8 @@ export default function Home() {
                 <Box
                   bgcolor={
                     message.role === "assistant"
-                    ? "primary.main"
-                    : "secondary.main"
+                      ? "primary.main"
+                      : "secondary.main"
                   }
                   color="white"
                   borderRadius={16}
@@ -65,18 +113,16 @@ export default function Home() {
         </Stack>
 
         {/* The Textfield of the chatbox */}
-        <Stack
-          direction="row"
-          spacing={2}>
-            <TextField 
-              label="message"
-              fullWidth
-              value ={message}
-              onChange={(e) => setMessage(e.target.value)} 
-              />
-            <Button variant = "contained">
-              Send
-            </Button>
+        <Stack direction="row" spacing={2}>
+          <TextField
+            label="message"
+            fullWidth
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button variant="contained" onClick={sendMessage}>
+            Send
+          </Button>
         </Stack>
       </Stack>
     </Box>
