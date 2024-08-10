7ec82246b6861b9ac26939db4eed00718adba055
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2024-08-09 23:19:37 -0600

    Users can signin with their email/pass

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,33 +1,35 @@
 "use client"
 import { useState } from 'react';
-import '../globals.css';
-import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth } from "@/app/firebase/config";
+import { useRouter } from "next/navigation";
 
-const SignUp = () => {
+const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [signInWithEmailAndPassword, user, loading, error] = useSignInWithEmailAndPassword(auth);
+  const router = useRouter();
 
-  const [createUserWithEmailAndPassword, user, loading, error] = useCreateUserWithEmailAndPassword(auth);
-
-  const handleSignOut = async (e) => {
-    e.preventDefault();
-
+  const handleSignIn = async (event) => {
+    event.preventDefault();
     try {
-      const res = await createUserWithEmailAndPassword(email, password);
-      console.log({ res });
-      setEmail('');
-      setPassword('');
+      const res = await signInWithEmailAndPassword(email, password);
+      console.log({res})
+      if (res) {
+        setEmail("");
+        setPassword("");
+        router.push("/");
+      }
     } catch (e) {
-      console.error(e);
+      console.error('Error signing in:', e.message);
     }
   };
 
   return (
     <div className="container">
       <div className="form-container">
-        <h2 className="form-title">Sign Up</h2>
-        <form onSubmit={handleSignOut} className="form">
+        <h2 className="form-title">Sign In</h2>
+        <form onSubmit={handleSignIn} className="form">
           <div className="form-group">
             <label htmlFor="email" className="form-label">Email</label>
             <input
@@ -52,13 +54,19 @@ const SignUp = () => {
               className="form-input"
             />
           </div>
-          <button type="submit" className="form-button">
-            Sign Up
+          <button type="submit" className="form-button" disabled={loading}>
+            {loading ? "Signing In..." : "Sign In"}
           </button>
         </form>
+        {error && (
+          <div className="error-message">
+            <p>Error: {error.message}</p>
+            {error.code && <p>Code: {error.code}</p>}
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
-export default SignUp;
+export default SignIn;
